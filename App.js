a53c24d75c6e2d02a490c685478c62c15e770f03
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-06 15:54:17 -0400

    added new item section

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Image ,FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem'
+import AddItem from './components/AddItem';
+// import { uuid } from 'uuidv4';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text: 'milk'},
+    {id: 2, text: 'Cheese'},
+    {id: 3, text: 'Ham'},
+    {id: 4, text: 'Steak'}
+  ])
+
+  const deleteItem = (id) => {
+    setItems(prev => {
+      return prev.filter(item => item.id != id)
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header title={'Todo List'}/>
+      <FlatList data={items} renderItem={({item}) => <ListItem item={item} deleteItem={deleteItem} /> }/>
     </View>
   )
 }
